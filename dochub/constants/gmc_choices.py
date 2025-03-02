72822b70367d7820cda74301617118886723740a
eatyourpeas <simon@eatyourpeas.co.uk>
2025-03-02 00:05:32 +0000

    start parsing csv into model - not finished

---
+++ dochub/constants/gmc_choices.py
@@ -0,0 +1,123 @@
+INDICATOR_CHOICES = (
+    (1, 'Supportive Environment'),
+    (2, 'Feedback'),
+    (3, 'Teamwork'),
+    (4, 'Regional Teaching'),
+    (5, 'Facilities'),
+    (6, 'Educational Supervision'),
+    (7, 'Adequate Experience'),
+    (8, 'Reporting Systems'),
+    (9, 'Clinical Supervision'),
+    (10, 'Induction'),
+    (11, 'Study Leave'),
+    (12, 'Work Load'),
+    (13, 'Clinical Supervision out of hours'),
+    (14, 'Local Teaching'),
+    (15, 'Overall Satisfaction'),
+    (16, 'Educational Governance'),
+)
+
+BENCHMARK_CHOICES = (
+    (0, 'General Practice'), 
+    (1,'General psychiatry'),
+    (2,'Acute Internal Medicine'), 
+    (3,'Anaesthetics'),
+    (4, 'Audio vestibular medicine'), 
+    (5,'Cardio-thoracic surgery'),
+    (6,'Cardiology'), 
+    (7, 'Chemical pathology'),
+    (8, 'Child and adolescent psychiatry'), 
+    (9,'Clinical genetics'),
+    (10,'Clinical oncology'), 
+    (11,'Clinical radiology'),
+    (12,'Community Child Health'),
+    (13,'Community Sexual and Reproductive Health'), 
+    (14,'Dermatology'),
+    (15,'Emergency Medicine'), 
+    (16,'Endocrinology and diabetes mellitus'),
+    (17, 'Forensic psychiatry'), 
+    (18, 'Gastroenterology'),
+    (19,'General (internal) medicine'), 
+    (20, 'General surgery'),
+    (21, 'Genito-urinary medicine'), 
+    (22, 'Geriatric medicine'), 
+    (23, 'Haematology'),
+    (24, 'Histopathology'), 
+    (25, 'Immunology'), 
+    (26, 'Infectious diseases'),
+    (27, 'Intensive care medicine'),
+    (28, 'Interventional Radiology'),
+    (29, 'Medical microbiology',) 
+    (30, 'Medical oncology'),
+    (31, 'Medical psychotherapy',) 
+    (32, 'Neonatal Medicine',) 
+    (33, 'Neurology'),
+    (34, 'Neurosurgery',) 
+    (35, 'Obstetrics and gynaecology'),
+    (36, 'Occupational medicine',) 
+    (37, 'Old age psychiatry',) 
+    (38, 'Ophthalmology'),
+    (39, 'Oral and maxillo-facial surgery',) 
+    (40, 'Otolaryngology'),
+    (41, 'Paediatric Emergency Medicine'),
+    (42, 'Paediatric Intensive Care Medicine',) 
+    (43, 'Paediatric Nephrology'),
+    (44, 'Paediatric Respiratory Medicine',) 
+    (45, 'Paediatric cardiology'),
+    (46, 'Paediatric surgery',) 
+    (47, 'Paediatrics',) 
+    (48, 'Palliative medicine'),
+    (49, 'Plastic surgery',) 
+    (50, 'Psychiatry of learning disability'),
+    (51, 'Rehabilitation medicine',) 
+    (52, 'Renal medicine'),
+    (53, 'Respiratory Medicine',) 
+    (54, 'Rheumatology'),
+    (55, 'Sport and exercise medicine',) 
+    (56, 'Stroke Medicine'),
+    (57, 'Trauma and orthopaedic surgery',) 
+    (58, 'Urology', 'Vascular surgery'),
+    (59, 'Allergy',) 
+    (60, 'Clinical neurophysiology'),
+    (61, 'Clinical pharmacology and therapeutics',) 
+    (62, 'Hepatology'),
+    (63, 'Liaison Psychiatry',) 
+    (64, 'Medical ophthalmology',) 
+    (65, 'Medical virology'),
+    (66, 'Nuclear medicine',) 
+    (67, 'Paediatric Diabetes and Endocrinology'),
+    (68, 'Paediatric Neurology',) 
+    (69, 'Paediatric Oncology'),
+    (70, 'Paediatric and perinatal pathology',) 
+    (71, 'Public health medicine'),
+    (72, 'Addiction Psychiatry',) 
+    (73, 'Gynaecological Oncology'),
+    (74, 'Rehabilitation Psychiatry',) 
+    (75, 'Diagnostic neuropathology'),
+    (76, 'Maternal and Fetal Medicine'),
+    (77, 'Paediatric Gastroenterology, Hepatology and Nutrition'),
+    (78, 'Paediatric Immunology, Infectious Diseases and Allergy'),
+    (79, 'Paediatric Neurodisability'),
+     (80, 'Paediatric Palliative Medicine'),
+    (81, 'Forensic histopathology'),
+    (82, 'Paediatric Inherited Metabolic Medicine'),
+    (83, 'Paediatric Rheumatology'), 
+    (84, 'Metabolic Medicine'),
+    (85, 'Pre-hospital Emergency Medicine'),
+    (86, 'Sexual and Reproductive Health'), 
+    (87, 'Urogynaecology'),
+    (88, 'Neuropathology'), 
+    (89, 'Medical microbiology and virology'),
+    (90, 'Aviation and space medicine'), 
+    (91, 'Pharmaceutical medicine'),
+    (92, 'All General Practice Posts'),
+    (93, 'All Psychiatry Posts'),
+    (94, 'All Medicine Posts'),
+    (95, 'All Anaesthetists Posts'),
+    (96, 'All Surgery Posts'),
+    (97, 'All Pathology Posts'),
+    (98, 'All Radiology Posts'),
+    (99, 'All Paediatrics and Child Health Posts'),
+    (100, 'All Obstetrics and Gynaecology Posts'),
+    (101, 'All Emergency Medicine Posts'),
+)
--- dochub/general_functions/excel_functions.py
+++ dochub/general_functions/excel_functions.py
@@ -1,6 +1,9 @@
 import os
 import pandas as pd
 from django.conf import settings
+from django.apps import apps
+
+from ..constants import INDICATOR_CHOICES, BENCHMARK_CHOICES
 
 def get_unique_specialties():
     """
@@ -16,7 +19,54 @@ def get_unique_specialties():
     file_path = os.path.join(settings.BASE_DIR,'dochub', 'data', 'nts-ltd-postspecbysite-2024_xlsx-107341611.xlsx')
     df = pd.read_excel(file_path)
 
-    # print a list of all unqiue specialties
-    print(df['Post Specialty'].unique())
+    # import the data into django model
+    GMC = apps.get_model('general_functions', 'GMC')
+
+    # column names
+    headers = ['#Geo_LETB_Deanery', 'Post_Specialty', 'Trust_Board', 'Site', 'Indicator', 'Year', 'Outcome', 'Response_Rate', 'Mean', 'CI_Lower', 'CI_Upper', 'N_Range', 'Benchmark_Name', 'National_Mean', 'National_Min', 'National_Q1', 'National_Median', 'National_Q3', 'National_Max', 'National_CI_Lower', 'National_CI_Upper', 'National_N']
+
+    # The Site column unfortunately has the ODS code appended to the name of the Site heading after a -
+
+    # iterate through the dataframe and save each row as an instance of the GMC model
+    # note the specialty and the indicator map to the choices in the GMC model
+
+    for index, row in df.iterrows():
+        deanery_name, post_specialty, trust_board, site, indicator, year, outcome, response_rate, mean, ci_lower, ci_upper, n_range, benchmark_name, national_mean, national_min, national_Q1, national_median, national_Q3, national_max, national_ci_lower, national_ci_upper, national_n = row
+
+        # get the specialty
+        post_specialty = post_specialty.strip()
+        # get the key of the indicator
+        indicator = [choice[0] for choice in INDICATOR_CHOICES if choice[1] == indicator][0]
+        # get the key of the benchmark
+        benchmark_name = [choice[0] for choice in BENCHMARK_CHOICES if choice[1] == benchmark_name][0]
+
+
+        # save the instance
+        GMC.objects.create(
+            deanery_name=deanery_name,
+            post_specialty=post_specialty,
+            trust_board=trust_board,
+            site=site,
+            site_ods_code=site.split('-')[1],
+            indicator=indicator,
+            year=year,
+            outcome=outcome,
+            response_rate=response_rate,
+            mean=mean,
+            ci_lower=ci_lower,
+            ci_upper=ci_upper,
+            n_range=n_range,
+            benchmark_name=benchmark_name,
+            national_mean=national_mean,
+            national_min=national_min,
+            national_Q1=national_Q1,
+            national_median=national_median,
+            national_Q3=national_Q3,
+            national_max=national_max,
+            national_ci_lower=national_ci_lower,
+            national_ci_upper=national_ci_upper,
+            national_n=national_n
+        )
+    
     return df
 
